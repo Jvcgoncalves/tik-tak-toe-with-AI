dc556d5bc428358dc81111470e418d14efc9e885
goncalves <goncalvesdacosta05@gmail.com>
2024-08-19 21:29:43 -0300

    adding reset button

--- src/scripts/handleUserClick/handleUserClick.js
+++ src/scripts/handleUserClick/handleUserClick.js
@@ -1,19 +1,31 @@
 let availableChoices = ['', '', '', '', '', '', '', '', ''];
+const availableBoxes = document.querySelectorAll(".hidden-input-label");
 
 export default function addUserClick() {
-  const availableBoxes = document.querySelectorAll(".hidden-input-label");
-
   availableBoxes.forEach(availableBox => {
     availableBox.addEventListener("click",event =>  handleUserClick(event))
   })
 }
 
 function handleUserClick(event) {
-  const inputTrigged = event.target;
-  
-  if(inputTrigged.id) {
-    const arrayPositionToFill = +inputTrigged.id.match(/\d/g);
+  const labelTrigged = event.target;
+  const inputFromLabel = document.getElementById(`box-${+labelTrigged.htmlFor.match(/\d/g)}`);
 
-    availableChoices[arrayPositionToFill] = "X"
+  if(inputFromLabel) {
+    const arrayPositionToFill = +inputFromLabel.id.match(/\d/g);
+
+    inputFromLabel.setAttribute("disabled", true);
+    availableChoices[arrayPositionToFill] = "X";
+    labelTrigged.innerText = "X";
   }
+}
+
+export function addResetGame() {
+  const resetButton = document.getElementById("reset-button");
+
+  resetButton.addEventListener("click", () => {
+    availableChoices = ['', '', '', '', '', '', '', '','']
+
+    availableBoxes.forEach(box => box.innerText = "")
+  })
 }
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,12 @@
 import addInputHandler from "./handleInput/handleInputFunction.js";
-import addUserClick from "./handleUserClick/handleUserClick.js";
+import addUserClick, { addResetGame } from "./handleUserClick/handleUserClick.js";
 import addStartGame from "./startGame/startGame.js";
 
 function initFunctions() {
   addInputHandler();
   addUserClick();
   addStartGame();
+  addResetGame();
 }
 
 initFunctions();
